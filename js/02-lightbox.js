13d1c7fbd9390390071a6bc48d200cc733a7665e
zVladimirz <tzxdlo@gmail.com>
2022-05-03 23:10:04 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -13,7 +13,7 @@ const imgItems = galleryItems
 return `
 
 <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
+  <img class="gallery__image" src="${preview}" alt="${description}" title="Beautiful Image"/>
 </a>
 
 `;
