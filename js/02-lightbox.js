8cc81913e8e340ff0e624c1ed071a8cd1ee433ff
zVladimirz <tzxdlo@gmail.com>
2022-05-03 21:49:10 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+
+
+
+const imgItems = galleryItems
+.map (({preview,original,description}) => {
+return `
+
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+
+`;
+})
+.join('');
+
+
+
+
+
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = imgItems;
+
+
